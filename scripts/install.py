4fcca8ed300530f2eead8705d193e90a92dead32
Kim van der Riet <kimp@vanderriet.com>
2020-12-16 15:48:24 -0500

    ENTMQCL-1745: WIP - Removed ficticious qpid-proton rpm from install list

--- scripts/install.py
+++ scripts/install.py
@@ -4,6 +4,5 @@ import fortworth as _fortworth
 
 if __name__ == "__main__":
     _fortworth.rpm_install_tag_packages('rh-qpid-proton-dist-el7', 'master', 'tested',
-                                        'qpid-proton',
                                         'qpid-proton-cpp-devel',
                                         'python-qpid-proton')
