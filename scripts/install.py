4f1d50e924ed30bb52c5076646610ab8d6823e23
Kim van der Riet <kimp@vanderriet.com>
2020-12-16 12:29:24 -0500

    ENTMQCL-1745: WIP - Added fortworth debug for rpm_install_tag_packages() which is not working

--- scripts/install.py
+++ scripts/install.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import fortworth as _fortworth
-import sys
 
 if __name__ == "__main__":
     _fortworth.rpm_install_tag_packages('rh-qpid-proton-dist-el7', 'master', 'tested',
-        'qpid-proton', 'qpid-proton-cpp-devel', 'python-qpid-proton')
+                                        'qpid-proton',
+                                        'qpid-proton-cpp-devel',
+                                        'python-qpid-proton')
