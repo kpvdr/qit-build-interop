daed8f190f032d1579010b8815ad3d656cd0b460
Kim van der Riet <kimp@vanderriet.com>
2021-01-15 12:05:04 -0500

    Changed proton rhel7 to proton rhel8 for install

--- scripts/install.py
+++ scripts/install.py
@@ -3,6 +3,6 @@
 import fortworth as _fortworth
 
 if __name__ == "__main__":
-    _fortworth.rpm_install_tag_packages('rh-qpid-proton-dist-el7', 'master', 'tested',
+    _fortworth.rpm_install_tag_packages('rh-qpid-proton-dist-el8', 'master', 'tested',
                                         'qpid-proton-cpp-devel',
                                         'python-qpid-proton')
