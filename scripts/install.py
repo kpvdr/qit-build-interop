b6d2f72821b6ea387b0ce949a8eed408f10a6168
Kim van der Riet <kimp@vanderriet.com>
2021-01-15 12:17:15 -0500

    Fixed rhel8 proton package name

--- scripts/install.py
+++ scripts/install.py
@@ -5,4 +5,4 @@ import fortworth as _fortworth
 if __name__ == "__main__":
     _fortworth.rpm_install_tag_packages('rh-qpid-proton-dist-el8', 'master', 'tested',
                                         'qpid-proton-cpp-devel',
-                                        'python-qpid-proton')
+                                        'python3-qpid-proton')
